a6746d70ec6735740cdc382b82be872a15538fdc
Tim Gabets <tim@gabets.ru>
2017-05-11 12:25:25 +0300

    v1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tracetools',
-      version='0.2',
+      version='1.0',
       
       description='Trace toolkit to print formatted traces of binary data',
       long_description=open('README').read(),
