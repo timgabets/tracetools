f9b9ef9fc7b6c393be7a6fe075540397d4813f25
Tim Gabets <tim@gabets.ru>
2017-05-11 12:10:14 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(name='tracetools',
+      version='0.01',
+      
+      description='Trace toolkit to print formatted traces of hex- and binary data',
+      long_description=open('README').read(),
+      
+      classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Operating System :: OS Independent',
+        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        
+        'Topic :: Communications',
+        'Intended Audience :: Developers',
+      ],
+      
+      keywords='trace hex',
+      
+      url='https://github.com/timgabets/tracetools',
+      author='Tim Gabets',
+      author_email='tim@gabets.ru',
+      
+      license='LGPLv2',
+      packages=['tracetools'],
+      install_requires=[],
+      zip_safe=True)
